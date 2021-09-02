0d793fd6ce22c6e3bdd4004654cd5d751c28a46d
NicolaRebecca <master9895@gmail.com>
2021-09-02 16:48:20 +0200

    header

---
+++ resources/views/templates/header.blade.php
@@ -0,0 +1,31 @@
+<div class="pre-nav">
+    <div class="container">
+        <div>DC POWER VISA</div>
+        <div>ADDITIONAL DC SITES</div>
+    </div>
+</div>
+
+<nav class="container">
+    <div class="nav-logo">
+        <img src="images/dc-logo.png" alt >
+    </div>
+
+    <div class="nav-menu">
+        <ul>
+            <li><a href="#">CHARACTERS</a></li>
+            <li><a href="#">COMICS</a></li>
+            <li><a href="#">MOVIES</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">GAMES</a></li>
+            <li><a href="#">COLLECTIBLES</a></li>
+            <li><a href="#">VIDEOS</a></li>
+            <li><a href="#">FANS</a></li>
+            <li><a href="#">NEWS</a></li>
+            <li><a href="#">SHOP</a></li>
+        </ul>
+        
+    </div>
+
+    <input type="text" placeholder="Search">
+
+</nav>
